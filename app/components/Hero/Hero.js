abb6b3735a6e565856be04abae5d2fd096832a63
Oscar Deb <jfsdev@triosoft.tech>
2023-10-30 18:33:16 -0700

    footer added

--- app/components/Hero/Hero.js
+++ app/components/Hero/Hero.js
@@ -1,39 +1,53 @@
 
 import Link from "next/link"
 import './Hero.css';
-import Image from "next/image";
+import img from "next/image";
 
 const Hero = () => {
-    return (
-      <section className="hero container">
-      <div className="header-content">
-          <div className="digital-product">
-            <span>DIGITAL</span>
-            <span className="product"> PRODUCT</span>
-          </div>
-          <b className="development-agency">DEVELOPMENT AGENCY</b>
-          <div className="global-development-company">
-            Global development company that redefines brands, enhances business
-            growth and empowers global enterprises
-          </div>
-          <div className="schedule-reframe">
-            <div className="schedule-a-call">
-              <img
-                className="streamlinecomputer-webcam-vid-icon"
-                alt=""
-                src="./public/streamlinecomputerwebcamvideoworkvideomeetingcameracompanyconferenceoffice.svg"
-              />
+  return (
+    <>
+    <div className="header-content">
+      <div className="digital-product">
+        <span>DIGITAL</span>
+        <span className="product"> PRODUCT</span>
+      </div>
+      <b className="development-agency">DEVELOPMENT AGENCY</b>
+      <div className="global-development-company">
+        Global development company that redefines brands, enhances business
+        growth and empowers global enterprises
+      </div>
+      <div className="schedule-reframe">
+        <div className="schedule-a-call">
 
-              <div className="schedule-a-call1">Schedule A Call</div>
-            </div>
-          </div>
+          <div className="schedule-a-call1">Schedule A Call</div>
         </div>
-{/*       
-      <div className="header-image">
-          <img className="image-1-icon" alt="" src="./public/img/image-1@2x.png" />
-        </div> */}
-    </section>
-    );
+      </div>
+    </div>
+
+<div className="rectangle-parent">
+<div className="frame-child"></div>
+<img className="noormahal-icon" alt="" src="./noormahal@2x.png" />
+
+<img className="antooba-icon" alt="" src="./antooba@2x.png" />
+
+<img
+  className="nearest-laundry-icon"
+  alt=""
+  src="./nearest-laundry@2x.png"
+/>
+
+<img className="dolil-icon" alt="" src="./dolil@2x.png" />
+
+<img
+  className="colonel-saab-icon"
+  alt=""
+  src="./colonel-saab@2x.png"
+/>
+
+<img className="neemsah-icon" alt="" src="./neemsah@2x.png" />
+</div>
+</>
+  );
 }
 
 export default Hero;
